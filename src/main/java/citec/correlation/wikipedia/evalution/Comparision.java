858a907fe1daad00f6e07f0a48b6ea48dcd6f078
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2020-10-21 12:52:17 +0200

    added reciprocal mean

--- src/main/java/citec/correlation/wikipedia/evalution/Comparision.java
+++ src/main/java/citec/correlation/wikipedia/evalution/Comparision.java
@@ -8,9 +8,15 @@ package citec.correlation.wikipedia.evalution;
 import citec.correlation.wikipedia.qald.Unit;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import qa.qf.qcri.iyas.evaluation.ir.MeanReciprocalRank;
 
 /**
  *
@@ -19,31 +25,93 @@ import java.util.List;
 public class Comparision {
 
     public Comparision(String qaldFileName, String methodFileName) throws IOException {
-        List<LexiconUnit> lexiconUnit = getLexicon(methodFileName);
-        List<Unit> units = getQald(qaldFileName);
+        Map<String, LexiconUnit> lexicons = getLexicon(methodFileName);
+        Map<String, Unit> qald = getQald(qaldFileName);
+        Set<String> commonWords = Sets.intersection(qald.keySet(), lexicons.keySet());
+        System.out.println(commonWords);
+        this.comparisions(qald, lexicons, commonWords);
+    }
 
-        for (LexiconUnit LexiconUnit : lexiconUnit) {
-            System.out.println(LexiconUnit);
+    private Map<String, LexiconUnit> getLexicon(String methodFileName) throws IOException {
+        Map<String, LexiconUnit> lexicons = new TreeMap<String, LexiconUnit>();
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<LexiconUnit> lexiconUnits = mapper.readValue(Paths.get(methodFileName).toFile(), new TypeReference<List<LexiconUnit>>() {
+        });
+        for (LexiconUnit LexiconUnit : lexiconUnits) {
+            lexicons.put(LexiconUnit.getWord(), LexiconUnit);
         }
+
+        return lexicons;
+    }
+
+    private Map<String, Unit> getQald(String qaldFileName) throws IOException {
+        Map<String, Unit> qald = new TreeMap<String, Unit>();
+        ObjectMapper mapper = new ObjectMapper();
+        List<Unit> units = mapper.readValue(Paths.get(qaldFileName).toFile(), new TypeReference<List<Unit>>() {
+        });
         for (Unit unit : units) {
-            System.out.println(unit);
+            qald.put(unit.getWord(), unit);
+        }
+        return qald;
+    }
+
+    private double calculate(Map<String, Double> predict, Map<String, Boolean> goldRelevance) {
+        double predictedReciprocalRank
+                = MeanReciprocalRank.getReciprocalRank(predict, goldRelevance);
+        return predictedReciprocalRank;
+    }
+
+    private void comparisions(Map<String, Unit> qald, Map<String, LexiconUnit> lexicons, Set<String> commonWords) {
+        for (String word : commonWords) {
+            Unit unit = qald.get(word);
+            String sparql = "dbo:country res:Australia";
+            LexiconUnit LexiconUnit = lexicons.get(word);
+            Map<String, Boolean> goldRelevance = new HashMap<String, Boolean>();
+            Map<String, Double> predict = new HashMap<String, Double>();
+            for (Integer rank : LexiconUnit.getEntityInfos().keySet()) {
+                List<String> pairs = LexiconUnit.getEntityInfos().get(rank);
+                predict.put(pairs.get(0).split("=")[1], Double.parseDouble(pairs.get(1).split("=")[1]));
+            }
+            for (String pairT : predict.keySet()) {
+                if (pairT.contains(sparql)) {
+                    goldRelevance.put(pairT, Boolean.TRUE);
+                } else {
+                    goldRelevance.put(pairT, Boolean.FALSE);
+                }
+
+            }
+            Double predictedReciprocalRank = this.calculate(predict, goldRelevance);
+            System.out.println(word + " predictedReciprocalRank: " + predictedReciprocalRank);
+
         }
 
-    }
+        /* for (String key : qald.keySet()) {
+            Unit unit = qald.get(key);
+            Map<String, Boolean> goldRelevance = new HashMap<String, Boolean>();
+            if (key.contains(word)) {
+                if (lexicons.containsKey(key)) {
+                    String sparql = "dbo:country res:Australia";
+                    LexiconUnit LexiconUnit = lexicons.get(key);
+                    Map<String, Double> predict = new HashMap<String, Double>();
+                    for (Integer rank : LexiconUnit.getEntityInfos().keySet()) {
+                        List<String> pairs = LexiconUnit.getEntityInfos().get(rank);
+                        predict.put(pairs.get(0).split("=")[1], Double.parseDouble(pairs.get(1).split("=")[1]));
+                    }
+                    for (String pairT : predict.keySet()) {
+                        if (pairT.contains(sparql)) {
+                            goldRelevance.put(pairT, Boolean.TRUE);
+                        } else {
+                            goldRelevance.put(pairT, Boolean.FALSE);
+                        }
 
-    private List<LexiconUnit> getLexicon(String methodFileName) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        List<LexiconUnit> lexiconUnit = mapper.readValue(Paths.get(methodFileName).toFile(), new TypeReference<List<LexiconUnit>>() {
-        });
-        return lexiconUnit;
-    }
+                    }
+                    Double predictedReciprocalRank = this.calculate(predict, goldRelevance);
+                    System.out.println("predictedReciprocalRank: " + predictedReciprocalRank);
 
-    private List<Unit> getQald(String qaldFileName) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-         List<Unit> lexiconUnit = mapper.readValue(Paths.get(qaldFileName).toFile(), new TypeReference<List<Unit>>() {
-        });
-
-        return lexiconUnit;
+                }
+            }
+        }*/
     }
 
 }
